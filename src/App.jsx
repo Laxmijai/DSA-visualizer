41a3a36628f1bf810feeabfbeed8849edadeb238
Laxmi Jaiswal <knit2206.ac.in@gmail.com>
2025-11-30 15:30:44 +0530

    adding routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Navbar from "./HomePage/Navbar"
+import HeroSection from "./HomePage/HeroSection"
+import Footer from "./HomePage/Footer"
+import Index from "./AlgorithmVisualizer"
 
 function App() {
   return (
     <>
+      
+     <BrowserRouter>
+     <Navbar/>
+     <Routes>
+     <Route path="/" element={<HeroSection/>}></Route> 
+     <Route path="/visualise" element= {<Index/>}></Route>
+     </Routes>
+      <Footer/>
+     </BrowserRouter>
+     
+      
     </>
   )
 }
